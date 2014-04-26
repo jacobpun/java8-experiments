45637fd6a1e8a70118176eeeba99e56a1191914c
Punnoose <punnoosekuttyj@yahoo.com>
2014-04-25 23:46:53 -0400

    Variable Scoping Demo

---
+++ LambdaExpressions/src/main/java/org/punnoose/java8lambda/demo14/LambdaExpressionsDemo14.java
@@ -0,0 +1,35 @@
+package org.punnoose.java8lambda.demo14;
+
+/**
+ * Variable Scoping Demo
+ * @author "Punnoose Pullolickal"
+ *
+ */
+public class LambdaExpressionsDemo14 {
+
+	private int instanceVariable=0;
+	
+	public void foo(){
+		int localVariable =0;
+		
+		//bar(10, localVariable -> System.out.print(localVariable)); // Illegal -> Variables having duplicate name
+		
+		//bar(10, i->{double localVariable =0;}); //Illegal -> Variables having duplicate name
+		
+		//bar (10, i-> {localVariable = 10;}); //Illegal -> local variables are effectively final 
+		
+		bar (10, i-> {int j = i+localVariable;}); //Legal
+		
+		bar(10, i-> this.instanceVariable++); //Legal
+		
+	}
+	
+	public void bar(int i, MyFunction function){
+		function.myFunction(i);
+	}
+
+}
+
+interface MyFunction{
+	void myFunction(int i);
+}
