2fb0089c5ba386b2fa5770e3ba74af0e6b69a58d
Punnoose <punnoosekuttyj@yahoo.com>
2014-04-26 08:43:09 -0400

    updated comments

--- LambdaExpressions/src/main/java/org/punnoose/java8lambda/demo14/LambdaExpressionsDemo14.java
+++ LambdaExpressions/src/main/java/org/punnoose/java8lambda/demo14/LambdaExpressionsDemo14.java
@@ -12,15 +12,15 @@ public class LambdaExpressionsDemo14 {
 	public void foo(){
 		int localVariable =0;
 		
-		//bar(10, localVariable -> System.out.print(localVariable)); // Illegal -> Variables having duplicate name
+		//bar(10, localVariable -> System.out.print(localVariable)); // <- Illegal; Variables having duplicate name
 		
-		//bar(10, i->{double localVariable =0;}); //Illegal -> Variables having duplicate name
+		//bar(10, i->{double localVariable =0;}); // <- Illegal; Variables having duplicate name
 		
-		//bar (10, i-> {localVariable = 10;}); //Illegal -> local variables are effectively final 
+		//bar (10, i-> {localVariable = 10;}); // <- Illegal; local variables are effectively final 
 		
-		bar (10, i-> {int j = i+localVariable;}); //Legal
+		bar (10, i-> {int j = i+localVariable;}); // <- Legal; local variables can be used in lambdas
 		
-		bar(10, i-> this.instanceVariable++); //Legal
+		bar(10, i-> this.instanceVariable++); // <- Legal; Instance variables can be updates
 		
 	}
 	
@@ -30,6 +30,7 @@ public class LambdaExpressionsDemo14 {
 
 }
 
+@FunctionalInterface
 interface MyFunction{
 	void myFunction(int i);
 }
