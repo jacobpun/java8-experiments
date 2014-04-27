746000c926ca522b851b7c8e7bc5db1c67094236
Punnoose <punnoosekuttyj@yahoo.com>
2014-04-27 16:26:22 -0400

    Added Demo2(Map)

---
+++ Streams/src/main/java/org/punnoose/java8streams/demo2/StreamsDemo2.java
@@ -0,0 +1,22 @@
+package org.punnoose.java8streams.demo2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.punnoose.java8streams.commonclasses.Person;
+
+/**
+ * Map Demo
+ * @author "Punnoose Pullolickal"
+ *
+ */
+public class StreamsDemo2 {
+	
+	public static void main(String[] args) {
+
+		List<String> peopleNames = Arrays.asList(new String []{"Mike", "John"});
+		peopleNames.stream().map(Person::new).forEach(System.out::println);;
+	
+	}
+
+}
